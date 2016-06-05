15bd37d6301422c17d1226bd6a0616f089cfff26
David <dawid.necka@gmail.com>
2016-06-06 01:29:12 +0200

    Uploading first version of programs, just random characters in array.

--- SharedMemory/src/SharedMemory.c
+++ SharedMemory/src/SharedMemory.c
@@ -10,8 +10,39 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	srand(time(NULL));
+
+	int size = 40;
+
+	//Shared memory
+	int shmid, i;
+	char *buf;
+	shmid = shmget(45281, size*sizeof(char), IPC_CREAT|0600);
+
+	if(shmid == -1)
+	{
+		perror("Creating shared memory");
+	}
+
+	buf = shmat(shmid, NULL,0);
+	if(buf == NULL)
+	{
+		perror("Attaching shared memory segment");
+		exit(1);
+	}
+
+	char a;
+	for(i=0;i<size;i++)
+	{
+		a = 'A' + (random() % 26);
+		buf[i] = a;
+	}
+
 	return EXIT_SUCCESS;
 }
